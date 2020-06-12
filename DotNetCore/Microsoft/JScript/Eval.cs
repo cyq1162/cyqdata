870413facdc03cfb5ba70ca2560aad891f6f68d8
Aster <272657997@qq.com>
2020-06-12 18:13:43 +0800

    更换nuget引用：Microsoft.ClearScript 为 Z.Expressions ,降低最低版本依赖（3.1到2.1）。

--- DotNetCore/Microsoft/JScript/Eval.cs
+++ DotNetCore/Microsoft/JScript/Eval.cs
@@ -1,19 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Microsoft.ClearScript.V8;
+﻿
 namespace Microsoft.JScript
 {
     internal class Eval
     {
-        static V8ScriptEngine v8; 
+        //static Microsoft.ClearScript.V8.V8ScriptEngine v8; 
         public static object JScriptEvaluate(string code, object obj)
         {
-            if (v8 == null)
-            {
-                v8 = new V8ScriptEngine();
-            }
-            return v8.Evaluate(code);
+            return Z.Expressions.Eval.Execute(code);
+            //if (v8 == null)
+            //{
+            //    v8 = new Microsoft.ClearScript.V8.V8ScriptEngine();
+            //}
+            //return v8.Evaluate(code);
         }
     }
 }
