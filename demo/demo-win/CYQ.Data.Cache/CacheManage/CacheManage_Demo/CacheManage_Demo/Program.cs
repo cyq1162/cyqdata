a57e64803224a0599ebe5c2dbbfc1569f58f2f8a
Aster <272657997@qq.com>
2022-09-26 18:20:03 +0800

    示例更新。

--- demo/demo-win/CYQ.Data.Cache/CacheManage/CacheManage_Demo/CacheManage_Demo/Program.cs
+++ demo/demo-win/CYQ.Data.Cache/CacheManage/CacheManage_Demo/CacheManage_Demo/Program.cs
@@ -11,14 +11,21 @@ namespace CacheManage_Demo
     {
         static void Main(string[] args)
         {
+            
             //说明：如果你确定缓存一定是在本机，使用：CacheManage cache= CacheManage.LocalInstance
             //如果只是缓存一般数据，将来有可能启用分布式时，用：CacheManage cache = CacheManage.Instance;
 
             //比如框架对一些表架构的元数据的缓存，用的是本机（速度快）：CacheManage.LocalInstance
             //而框架对于自动缓存（表的数据），用的是：CacheManage.Instance （将来随便分布式启用分散到各缓存服务器）
-
-            AppConfig.Cache.MemCacheServers = "127.0.0.1:11211";//配置启用MemCache,127.0.0.1:11212
-            CacheManage cache = CacheManage.Instance;
+            
+        }
+        /// <summary>
+        /// 本地缓存示例：（基本使用）
+        /// </summary>
+        static void LocalCache()
+        {
+            CacheManage cache = CacheManage.Instance;//自动获取缓存类型（默认本地缓存）。
+            //CacheManage cache = CacheManage.LocalInstance;//指定本地缓存
             if (!cache.Contains("a1"))
             {
                 cache.Set("a1", "a1", 0.1);
@@ -37,7 +44,7 @@ namespace CacheManage_Demo
             {
                 Console.WriteLine(table.Rows.Count);
             }
-          
+
             if (cache.CacheType == CacheType.LocalCache)//只能拿到本机的信息
             {
                 Console.WriteLine("缓存数：" + table.Rows.Count);
@@ -47,5 +54,27 @@ namespace CacheManage_Demo
             cache.Clear();//清除所有缓存
             Console.Read();
         }
+
+        /// <summary>
+        /// 分布式缓存：memcache 示例：
+        /// </summary>
+        static void MemCache()
+        {
+            //使用
+            AppConfig.Cache.MemCacheServers = "127.0.0.1:11211";//配置启用MemCache,127.0.0.1:11212
+            LocalCache();
+        }
+
+        /// <summary>
+        /// 分布式缓存：Redis 示例：
+        /// </summary>
+        static void RedisCache()
+        {
+            //使用分布式缓存：Redis
+            AppConfig.Cache.RedisServers = "127.0.0.1:6379";//配置启用Redis,127.0.0.1:6379
+           // AppConfig.Cache.RedisServers = "127.0.0.1:6379 - 123456";//配置启用Redis,127.0.0.1:6379，带密码：123456
+            LocalCache();
+        }
+
     }
 }
